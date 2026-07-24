e5dc51d17612ac4d7c6741d6f02e78edfaaf7f60
GitFlow Mobile <gitflowmobile@local>
2026-07-24 05:54:47 +0300

    fix and upgrades

--- app/src/main/java/com/willyshare/willykez/ui/screens/TransferringScreen.kt
+++ app/src/main/java/com/willyshare/willykez/ui/screens/TransferringScreen.kt
@@ -3,6 +3,7 @@ package com.willyshare.willykez.ui.screens
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
